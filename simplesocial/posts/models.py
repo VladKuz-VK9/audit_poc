0a438d4cfc6d723b47c9e089dbe60100f0eb8f53
Brian Arriaga <brian.arriaga@gmail.com>
2018-07-08 11:50:09 -0400

    Adding part 6 files and modifications

--- simplesocial/posts/models.py
+++ simplesocial/posts/models.py
@@ -1,3 +1,35 @@
+import misaka
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+
+from groups.models import Group
+
+User = get_user_model()
 
 # Create your models here.
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name='posts')
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(
+        Group, related_name='post', blank=True, null=True)
+
+    def __str__(self):
+        return self.message
+
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('post:single', kwargs={'username': self.username,
+                                              'pk': self.pk})
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['user', 'message']
